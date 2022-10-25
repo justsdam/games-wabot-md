83857d53fb44d7588d0ceae0536c1df4fa5b4c3d
justsdam <62846845+justsdam@users.noreply.github.com>
2022-10-25 23:59:07 +0700

    Update cmd-list.js

--- plugins/cmd-list.js
+++ plugins/cmd-list.js
@@ -2,10 +2,42 @@ import db from '../lib/database.js'
 
 let handler = async (m, { conn }) => {
     conn.reply(m.chat, `
-*DAFTAR HASH*
-\`\`\`
-${Object.entries(db.data.sticker).map(([key, value], index) => `${index + 1}. ${value.locked ? `(Terkunci) ${key}` : key} : ${value.text}`).join('\n')}
-\`\`\`
+🦉 : Halo sobat samupahita!, untuk menggunakan fitur yang ada pada bot, ketikkan command yang ada dalam list berikut ini.
+
+╭─「 🤷‍♂️ *Truth Or Dare* 」
+│ /truth
+│ /dare
+│ /hilih
+╰────
+╭─「 🦹 *Anonymous Chat* 」
+│ /start : _Untuk memulai chat_
+│ /next : _Beralih ke pengguna lain_
+│ /leave : _Untuk mengakhiri chat_
+╰────
+╭─「 🎮 *Game* 」
+│ /family100
+│ /tebakkata
+│ /siapakahaku
+╰────
+╭─「 🤖 *Sticker Maker* 」
+│ /stiker + _Reply Pesan_
+│ /stikergif + _Reply Pesan_
+│ /toimg + _Reply Pesan_
+│ /attp + _Kalimat Stiker_
+╰────
+╭─「 🔎 *Search* 」
+│ /google + _Keywords_
+│ /pinterest + _Keywords_
+│ /lirik + _Keywords_
+╰────
+╭─「 🔮 *Magic Ball* 」
+│ /apakah + _Pertanyaan Kalian_
+│ /kapankah + _Pertanyaan Kalian_
+│ /howganteng + _Nama Orang_
+│ /howcantik + _Nama Orang_
+│ /jadian
+│ /bucin
+╰────
 `.trim(), null, {
         mentions: Object.values(db.data.sticker).map(x => x.mentionedJid).reduce((a, b) => [...a, ...b], [])
     })
@@ -14,6 +46,6 @@ ${Object.entries(db.data.sticker).map(([key, value], index) => `${index + 1}. ${
 
 handler.help = ['listcmd']
 handler.tags = ['database']
-handler.command = ['listcmd']
+handler.command = [/^(menu|mulai|help|hai|\?)$/i]
 
 export default handler
